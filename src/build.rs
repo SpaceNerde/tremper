594b93ce7bc673cff32499883c192ed82e1c6fe0
Space Nerd <71553327+SpaceNerde@users.noreply.github.com>
2024-06-27 19:27:13 +0200

    ukfctluyflfyuluyfifyglkhjtggjzrxed

---
+++ src/build.rs
@@ -0,0 +1,4 @@
+fn main() {
+    println!("cargo:rustc-link-lib=C:\\NpcapSDK\\Lib\\x64\\wpcap.lib");
+    println!("cargo:rustc-link-lib=C:\\NpcapSDK\\Lib\\x64\\Packet.lib");
+}
---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use pcap::{Device, Capture};
+
+fn main() {
+    let device = pcap::Device::lookup()
+        .expect("device lookup failed")
+        .expect("no device available");
+    println!("Using device {}", device.name);
+
+    // Setup Capture
+    let mut cap = pcap::Capture::from_device(device)
+        .unwrap()
+        .immediate_mode(true)
+        .open()
+        .unwrap();
+
+    // get a packet and print its bytes
+    println!("{:?}", cap.next_packet());
+}
